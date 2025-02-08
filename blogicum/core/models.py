8b086071068cfe0fdc91e5c8da4e7aba7b3d5790
flammesh <radmir0two@gmail.com>
2025-02-08 05:01:27 +0300

    add 1 blank line after class dockstring

--- blogicum/core/models.py
+++ blogicum/core/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class PublishedDateModel(models.Model):
     """Абстрактная модель. Добавляет флаги is_published и created_at."""
+
     is_published = models.BooleanField(
         default=True, verbose_name='Опубликовано',
         help_text='Снимите галочку, чтобы скрыть публикацию.')
